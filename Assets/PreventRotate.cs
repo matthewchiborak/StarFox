06f78e4c8e2aeb089e1664d326ad936446ea3f18
matthewchiborak <matthewchiborak@hotmail.com>
2017-07-14 12:36:27 -0400

    Created a second scene to see if could transfer game elemenets to a new scene. It can be done but I feel there must be a more elegant way of doing it. Also started working on behaviours for the enemies

---
+++ Assets/PreventRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreventRotate : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.rotation = new Quaternion(0, 0, 0, 1);	
+	}
+}
---
+++ Assets/Scripts/EnemyBehaviours/RotateAroundPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPoint : MonoBehaviour {
+
+    public Vector3 pointToRotateAround;
+    public float radius;
+    public float startingAngle;
+    public float speed;
+
+    private float currentAngle;
+
+    void Start()
+    {
+        currentAngle = startingAngle;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(pointToRotateAround.x + radius * Mathf.Sin(currentAngle * (3.1415f / 180)), pointToRotateAround.y + radius * Mathf.Cos(currentAngle * (3.1415f / 180)), pointToRotateAround.z);
+        currentAngle += speed * Time.deltaTime;
+	}
+}
